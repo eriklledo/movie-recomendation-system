5309327131864238c26722eb813f8427e9befa96
Èrik Lledó <sonwerik@gmail.com>
2025-02-10 19:12:31 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,23 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        UserManager uM = new UserManager();
+        MovieRecomendationManager mRM = new MovieRecomendationManager();
         Scanner sc = new Scanner(System.in);
         System.out.println("""
                 
                 RECOMANADOR DE PEL·LÍCULES
-                
                 """);
 
-        boolean inLoginMenu = true;
-        while (inLoginMenu) {
+        boolean login = false;
+        while (!login) {
             System.out.println("""
                     
                     INICI DE SESSIÓ
                     
                     1. Nou usuari
                     2. Iniciar sessió
-                    3. Sortir de programa
+                    3. Entra com a convidat
+                    0. Sortir de programa
                     
                     """);
             System.out.print("Triï una opció: ");
@@ -29,47 +29,67 @@ public class Main {
             sc.nextLine();
 
             switch (option) {
-                case 1 -> createUser(sc, uM);
-                case 2 -> login(sc, uM);
-                case 3 -> inLoginMenu = false;
-                default -> System.out.println("Opció no vàlida. Torni a intentar-ho.");
+                case 1:
+                    createUser(sc, mRM);
+                    break;
+                case 2:
+                    login(sc, mRM);
+                    System.out.println(login);
+                    break;
+                case 3:
+                    login = true;
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    System.out.println("Opció no vàlida. Torni a intentar-ho.");
             }
         }
 
-        if (login(sc,uM)) {
-            boolean inMovieMenu = true;
-            while (inMovieMenu){
-                System.out.println("""
-                
-                PEL·LÍCULES DISPONIBLES
-                
-                1. Llistat de pel·lícules disponibles
-                2. Cercador de pel·lícules
-                3. Recomanador de pel·lícules (no disponible)
-                4. El meu Perfil
-                5. Tancar sessió
-                0. Sortir del programa
-                
-                """);
-                int option = sc.nextInt();
-                sc.nextLine();
 
-                switch (option) {
-                    case 1 -> System.out.println("1");
-                    case 2 -> System.out.println("2");
-                    case 3 -> System.out.println("3");
+        boolean inMovieMenu = true;
+        while (inMovieMenu) {
+            System.out.println("""
+                    
+                    PEL·LÍCULES DISPONIBLES
+                    
+                    1. Llistat de pel·lícules disponibles
+                    2. Cercador de pel·lícules
+                    3. Recomanador de pel·lícules (no disponible)
+                    4. El meu Perfil
+                    5. Tancar sessió
+                    0. Sortir del programa
+                    
+                    """);
+            System.out.print("Triï una opció: ");
+            int option = sc.nextInt();
+            sc.nextLine();
 
-                    case 4 -> {
-                        System.out.println("Ho sentim, aquesta funció, encara no está disponible");
-                        continue;
-                    }
-                    case 5 -> System.out.println("5");
-                    case 6 -> {
-                        break;
-                    }
-                    case 0 -> System.exit(0);
-                    default -> System.out.println("Opció no vàlida. Torni a intentar-ho.");
-                }
+            switch (option) {
+                case 1:
+                    System.out.println("1");
+                    break;
+
+                case 2:
+                    System.out.println("2");
+                    break;
+                case 4:
+                    System.out.println("Ho sentim, aquesta funció, encara no está disponible");
+                    break;
+                case 3:
+                    System.out.println("4");
+                    break;
+                case 5:
+                    System.out.println("5");
+                    break;
+                case 6:
+                    System.out.println("Tancant sessió...");
+                    inMovieMenu = false;
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    System.out.println("Opció no vàlida. Torni a intentar-ho.");
             }
         }
         sc.close();
@@ -89,20 +109,27 @@ public class Main {
 
     }
 
-    public static void defaultUsers() {
+    public static void defaultUsers(MovieRecomendationManager mRM) {
         User erik = new User(1, "Èrik", LocalDate.of(2004, 10, 15), "Spanish", "sonwerik", "sonwerik@mail.com", "miau");
+        mRM.addUser(erik);
     }
 
-    public static void createUser(Scanner sc, UserManager uM) {
-        String username;
+    public static void createUser(Scanner sc, MovieRecomendationManager mRM) {
+        String username = "";
         String email = "";
         String password = "";
 
-        System.out.print("Introdueixi un nom d'usuari: ");
-        username = sc.next();
+        while (true) {
+            System.out.print("Introdueixi un nom d'usuari: ");
+            username = sc.next();
+            if (mRM.checkUser(username)) {
+                System.out.println("Aquest nom d'usuari ja existeix. Torni-ho a intentar.");
+                continue;
+            }
+            break;
+        }
 
-        boolean inMail = true;
-        while (inMail) {
+        while (true) {
             System.out.print("Introdueixi el seu correu: ");
             email = sc.next();
 
@@ -110,11 +137,16 @@ public class Main {
                 System.out.println("Correu no vàlid. Torni a intentar-ho.\n");
                 continue;
             }
+
+            String finalEmail = email;
+            if (mRM.getUsers().stream().anyMatch(user -> user.getMail().equals(finalEmail))) {
+                System.out.println("Aquest correu electrònic ja està registrat. Torni-ho a intentar.");
+                continue;
+            }
             break;
         }
 
-        boolean inCreatePasswd = true;
-        while (inCreatePasswd) {
+        while (true) {
             System.out.print("Introdueixi una contrasenya: ");
             password = sc.next();
 
@@ -122,11 +154,7 @@ public class Main {
                 System.out.println("La contrasenya no compleix els requisits de seguretat.\n");
                 continue;
             }
-            break;
-        }
 
-        boolean inCheckPasswd = true;
-        while (inCheckPasswd) {
             System.out.print("Repeteixi la contrasenya: ");
             String confirmPassword = sc.next();
 
@@ -138,25 +166,33 @@ public class Main {
         }
 
         User user = new User(username, email, password);
-
-        uM.addUser(user);
+        mRM.addUser(user);
         System.out.println("\nUsuari creat correctament!");
     }
 
-    public static boolean login(Scanner sc, UserManager user) {
-        boolean login = true;
-        while (login) {
+    public static boolean login(Scanner sc, MovieRecomendationManager mRM) {
+        while (true) {
             System.out.print("Introdueixi el nom d'usuari o correu electronic: ");
             String checkUser = sc.next();
             System.out.print("Introdueixi un contrasenya: ");
             String checkPasswd = sc.next();
 
-            if (!user.checkUser(checkUser) || !user.checkPassword(checkPasswd)) {
-                login = false;
-                System.out.println("Usuari i/o contrsenya incorrectes\n");
+            if (!mRM.checkUser(checkUser) || !mRM.checkPassword(checkUser, checkPasswd)) {
+                System.out.println("\nUsuari i/o contrsenya incorrectes");
+
+                while (true) {
+                    System.out.println("Vols registrar-te? (si/no)");
+                    String answer = sc.next().toLowerCase();
+
+                    if (answer.equals("si")) return false;
+                    if (answer.equals("no")) break;
+                    System.out.println("Si us plau, respongui amb “si” o “no”.");
+                }
+
+            } else {
+                System.out.println("\nS'ha iniciat sessió correctament");
+                return true;
             }
         }
-        System.out.println("S'ha iniciat sessió correctament");
-        return login;
     }
 }
--- src/MovieRecomendationManager.java
+++ src/MovieRecomendationManager.java
@@ -1,2 +1,80 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class MovieRecomendationManager {
+    private Set<Movie> movies;
+    private Set<User> users;
+
+    public MovieRecomendationManager(Set<Movie> movies, Set<User> users) {
+        this.movies = new HashSet<>();
+        this.users = new HashSet<>();
+    }
+
+    public MovieRecomendationManager() {
+        this.movies = new HashSet<>();
+        this.users = new HashSet<>();
+    }
+
+    public Set<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(Set<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public void searchUserbyUsername(Scanner sc) {
+        String searchUser = sc.next();
+        for (User user : users) {
+            if (user.getUsername().equals(searchUser)) {
+                System.out.println(user);
+            } else if (user.getMail().equals(searchUser)) {
+                System.out.println(user);
+
+            } else {
+                System.out.println("Usuari no trobat.");
+            }
+        }
+    }
+
+    public boolean checkUser(String checkUser) {
+        return users.stream().anyMatch(user -> user.getUsername().equals(checkUser));
+    }
+
+    public boolean checkPassword(String checkUser, String checkPasswd) {
+        return users.stream()
+                .filter(user -> user.getUsername().equals(checkUser))
+                .anyMatch(user -> user.getPassword().equals(checkPasswd));    }
+
+
+    public void showAllUsers() {
+        for (User user : users) {
+            System.out.println(user);
+        }
+    }
+
+    public void removeUserById(int id) {
+        users.removeIf(user -> user.getId() == id);
+    }
+
+    @Override
+    public String toString() {
+        return "RecomenderManager{" +
+                "movies=" + movies +
+                ", users=" + users +
+                '}';
+    }
 }
