1cbb9bdcda9e196b367724f87ba1452607034857
Èrik Lledó <sonwerik@gmail.com>
2025-02-10 13:09:10 +0100

    creating classes

--- src/Iteracio1.java
+++ src/Iteracio1.java
@@ -1,7 +1,6 @@
-import java.sql.SQLOutput;
 import java.util.*;
 
-public class Main {
+public class Iteracio1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
